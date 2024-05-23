869ecc39add8f69da6e206bf4561f4b7a164a2da
victorpreston <prestonvictor25@gmail.com>
2024-05-24 02:55:06 +0300

    + add input form, I can now create activity and edit activity.

--- ts/main.js
+++ ts/main.js
@@ -1,4 +1,15 @@
-document.getElementById('habitForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
+document.querySelector('.create-activity').addEventListener('click', function() {
+    var form = document.querySelector('.activity-card');
+    form.style.display = 'block';
+    form.classList.add('slide-in');
+});
+
+document.querySelector('.cancel-button').addEventListener('click', function() {
+    var form = document.querySelector('.activity-card');
+    form.classList.remove('slide-in');
+    form.classList.add('slide-out');
+    setTimeout(function() {
+        form.style.display = 'none';
+        form.classList.remove('slide-out');
+    }, 500);
 });
